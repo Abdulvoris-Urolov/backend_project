5cda573dec3d1730c17cddca8a4b7a6ee59d0ad2
Abdulvorisjs <abdulvoris000urolov@gmail.com>
2021-11-23 23:52:36 +0500

    asas

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,4 @@
 const mongoose = require ("mongoose");
-// wwww by Abdulvoris
-const www = require("www")
 
 module.exports = function(){  mongoose.connect(process.env.MONGODB_URL, {})
     .then(() => console.log('Database connected!'))
