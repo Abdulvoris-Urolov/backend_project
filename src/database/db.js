6dae3eb429c640fdd405c73d2c3bc1f952cf1618
Abdulvorisjs <abdulvoris000urolov@gmail.com>
2021-11-23 23:16:52 +0500

    change by Abdurashid

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,9 @@
 const mongoose = require ("mongoose");
+const mysql = require("mysql")
 
 module.exports = function(){  mongoose.connect(process.env.MONGODB_URL, {})
     .then(() => console.log('Database connected!'))
     .catch(() => console.error('Database error'))
 }
+
+// Comment by Abdurashid
