113c4fc01c192872358ef38af20401d8aebf5a1f
Abdulvorisjs <abdulvoris000urolov@gmail.com>
2021-12-03 01:21:44 +0500

    true

--- src/database/db.js
+++ src/database/db.js
@@ -3,18 +3,9 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-<<<<<<< HEAD
-module.exports = function() {
-    mongoose.connect(process.env.MONGODB_URL, {})
-    .then(() => console.log('Database connected!'))
-    .catch(() => console.error('Database error'))
-}
-
-=======
 module.exports = function () {
   mongoose
     .connect(process.env.MONGODB_URL, {})
     .then(() => console.log("Database connected!"))
     .catch(() => console.error("Database ERROR"));
 };
->>>>>>> ba22ccd349610108ae82a61f7a427e671a88667d
