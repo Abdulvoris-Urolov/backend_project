2c1337d84f210e8cf2c39f88294c2535c31a9ede
Abdulvorisjs <abdulvoris000urolov@gmail.com>
2021-12-03 21:16:49 +0500

    connected

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const singup = require("../controller/auth");
 const signin = require("../controller/user");
 
-router.post("/singup", singup);
+router.post("/signup", singup);
 router.post("/signin", signin);
 
 module.exports = router;
