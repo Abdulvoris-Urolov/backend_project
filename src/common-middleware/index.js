541afd814036da1b62cf6240edd0ea299975371b
Abdulvorisjs <abdulvoris000urolov@gmail.com>
2021-12-08 15:10:50 +0500

    category change

--- src/common-middleware/index.js
+++ src/common-middleware/index.js
@@ -1,26 +1,26 @@
 const jwt = require("jsonwebtoken");
 
 exports.requireSignin = (req, res, next) => {
-  if (req.header.authorization) {
-    const token = req.authorization.split(" ")[1];
-    const user = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = user;
-  } else {
-    return res.status(400).json({ message: "Authorization required" });
+  if(req.headers.authorization){
+      const token = req.headers.authorization.split(" ")[1];
+      const user = jwt.verify(token, process.env.SECRET);
+      req.user = user;
+  }else{
+  return res.status(400).json({ message: 'Authorization required' });
   }
   next();
-};
-
-exports.adminMiddleware = (req, res, next) => {
-  if (req.user.role !== "admin") {
-    return res.status(400).json({ message: "Admin access denied" });
-  }
-  next();
-};
+}
 
 exports.userMiddleware = (req, res, next) => {
-  if (req.user.role !== "User") {
-    return res.status(400).json({ message: "User access denied" });
-  }
-  next();
-};
+    if(req.user.role !== 'user'){
+        return res.status(400).json({ message: 'User access denied'})
+    }
+    next();
+}
+
+exports.adminMiddleware = (req, res, next) => {
+    if(req.user.role !== 'admin'){
+        return res.status(400).json({ message: 'Admin access denied'})
+    }
+    next();
+}
--- src/controller/category.js
+++ src/controller/category.js
@@ -1,6 +1,28 @@
 const { Category } = require("../models/category");
 const slugify = require("slugify");
 
+function createCategory(categories, parentId = null) {
+  
+  const categoryList = [];
+  let category;
+  if(parentId == null){
+    category = categories.filter(cat => cat.parentId == undefined)
+  }else{
+    category = categories.filter(cat => cat.parentId == parentId)
+  }
+
+  for(let cate of category){
+    categoryList.push({
+      _id: cate._id,
+      name: cate.name,
+      slug: cate.slug,
+      children: createCategory(categories, cate._id)
+    });
+  }
+  
+  return categoryList;
+};
+
 const postcategory = (req, res) => {
   const categoryObj = {
     name: req.body.name,
@@ -20,12 +42,15 @@ const postcategory = (req, res) => {
   });
 };
 
-const getCategory = async (req, res) => {
+const getCategory = (req, res) => {
     Category.find({})
     .exec((error, categories) => {
         if(error) return res.status(400).json({error})
         if(categories){
-           res.status(200).json({categories})
+
+          const categoryList = createCategory(categories);
+
+           res.status(200).json({categoryList})
         }
     });
 }
