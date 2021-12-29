7df972a368a32a23bc68bd947b51104b077cb4c9
Abdulvorisjs <abdulvoris000urolov@gmail.com>
2021-12-29 23:33:44 +0500

    Page Api

--- src/models/page.js
+++ src/models/page.js
@@ -1,44 +1,41 @@
-
-  
 const mongoose = require('mongoose');
 const pageSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    banners: [
-        {
-            img: { type: String },
-            navigateTo: { type: String }
-        }
-    ],
-    products: [
-        {
-            img: { type: String },
-            navigateTo: { type: String }
-        }
-    ],
-    category: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Category', 
-        required: true ,
-        unique: true
-    },
-    createdBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+    unique: true
+  },
+  banners: [
+    {
+      img:{type:String},
+      navigateTo: { type: String }
     }
+  ],
+  products: [
+    {
+      img:{type:String},
+      navigateTo: { type: String }
+    }
+  ],
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
 },
 {
-    timestamps: { createdAt: "created_at" },
+  timestamps: { createdAt: "created_at" },
 });
-
 
 module.exports = mongoose.model('Page', pageSchema);
--- src/routes/admin/page.js
+++ src/routes/admin/page.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { upload, requireSignin, adminMiddleware } = require("../../common-middleware");
 const router = express.Router(); 
-const {createPage, getPage} = require("../../controller/admin/page")
+const {createPage, getPage} = require('../../controller/admin/page');
 
 router.post('/page/create', requireSignin, adminMiddleware, upload.fields([
   {name: 'banners'},
